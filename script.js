9a8490332e40878f04197c91b5e3b94bff5657b0
Usama-Yasir <usamayasir32@gmail.com>
2022-03-21 02:52:16 +0500

    update commit

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const APIURL = "https://api.hithub.com/users"
+const APIURL = "https://api.hithub.com/users";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
 async function getUser() {
     const resp = await fetch(APIURL + user);
@@ -8,5 +12,24 @@ async function getUser() {
 }
 
 function createUserCard(user) {
+    const card = document.createElement("div");
+    card.classList.add("card");
 
+    card.innerHTML = `
+    <div>
+        <img src="${user.avatar}" alt="${user.name}" />
+    </div>
+    <div>
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+
+        <ul>
+           <li>${user.followers}</li>
+           <li>${user.following}</li>
+           <li>${user.public_repos}</li>
+        </ul>
+    </div>
+    `;
+
+    main.appendChild
 }
